372bb44f4c9bc29479eaf0423dcbf3283529515e
Jeferson Almeida <jeffreys.sharp@outlook.com>
2025-03-22 15:55:12 -0300

    feat: adding unit test to RegisterUserValidator success

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterUserJson.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using Gauss.Investment.Communication.Requests;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestRegisterUserJsonBuilder
+    {
+        public static RequestRegisterUserJson Build()
+        {
+            return new Faker<RequestRegisterUserJson>()
+                .RuleFor(user => user.Name, (f) => f.Person.FirstName)
+                .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name))
+                .RuleFor(user => user.Password, (f) => f.Internet.Password());
+
+        }
+    }
+}
---
+++ tests/Validators.Test/User/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,22 @@
+﻿using CommonTestUtilities.Requests;
+using FluentAssertions;
+using Gauss.Investment.Application.UseCases.User.Register;
+
+namespace Validators.Test.User.Register
+{
+    public class RegisterUserValidatorTest
+    {
+        [Fact]
+        public void Success()
+        {
+
+            var validator = new RegisterUserValidator();
+
+            var request = RequestRegisterUserJsonBuilder.Build();
+            
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeTrue();
+        }
+    }
+}
