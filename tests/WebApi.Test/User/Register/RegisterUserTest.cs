3d6b425e5f026d7ea81c69acdaefb3021d462971
Jeferson Almeida <jeffreys.sharp@outlook.com>
2025-03-22 22:54:52 -0300

    feat: Integration testing success user registration

---
+++ tests/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -0,0 +1,33 @@
+﻿using CommonTestUtilities.Requests;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace WebApi.Test.User.Register
+{
+    public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _httpClient;
+        public RegisterUserTest(WebApplicationFactory<Program> factory) => _httpClient = factory.CreateClient();
+
+        [Fact]
+        public async Task Success()
+        {
+            var request = RequestRegisterUserBuilder.Build();
+            
+            var response = await _httpClient.PostAsJsonAsync("User", request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            responseData.RootElement.GetProperty("name").GetString().Should().NotBeNullOrWhiteSpace()
+                .And.Be(request.Name);
+
+        }
+    }
+}
