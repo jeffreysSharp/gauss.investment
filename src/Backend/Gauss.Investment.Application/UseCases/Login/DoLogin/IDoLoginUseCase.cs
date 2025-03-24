5f8569e7653047a4908a3fde80cc9beeff325e3f
Jeferson Almeida <jeffreys.sharp@outlook.com>
2025-03-24 11:39:33 -0300

    feat: Defining the business Rule for login with custom exception

---
+++ src/Backend/Gauss.Investment.Application/UseCases/Login/DoLogin/IDoLoginUseCase.cs
@@ -0,0 +1,10 @@
+﻿using Gauss.Investment.Communication.Requests;
+using Gauss.Investment.Communication.Responses;
+
+namespace Gauss.Investment.Application.UseCases.Login.DoLogin
+{
+    public interface IDoLoginUseCase
+    {
+        public Task<ResponseRegisteredUser> Execute(RequestLogin request);
+    }
+}
