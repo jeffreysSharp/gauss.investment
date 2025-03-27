640a546fd2a5d59f6ce9ee3843e96f02d17cb86c
Jeferson Almeida <jeffreys.sharp@outlook.com>
2025-03-27 15:25:22 -0300

    feat: Unit tests for updating the user profile

---
+++ tests/CommonTestUtilities/LoggedUser/LoggedUserBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Gauss.Investment.Domain.Entities;
+using Gauss.Investment.Domain.Services.LoggedUser;
+using Moq;
+
+namespace CommonTestUtilities.LoggedUser
+{
+    public class LoggedUserBuilder
+    {
+        public static ILoggedUser Build(User user)
+        {
+            var mock = new Mock<ILoggedUser>();
+
+            mock.Setup(x => x.User()).ReturnsAsync(user);
+
+            return mock.Object;
+        }
+    }
+}
---
+++ tests/CommonTestUtilities/Requests/RequestUpdateUserBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using Gauss.Investment.Communication.Requests;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestUpdateUserBuilder
+    {
+        public static RequestUpdateUser Build()
+        {
+            return new Faker<RequestUpdateUser>()
+                .RuleFor(user => user.Name, (f) => f.Person.FirstName)
+                .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name));
+        }
+    }
+}
