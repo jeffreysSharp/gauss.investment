c20ab97595fca2bbe1ed1ff1a91d540fceda9e7b
Jeferson Almeida <jeffreys.sharp@outlook.com>
2025-03-27 15:25:22 -0300

    feat: Unit tests for updating the user profile

---
+++ tests/CommonTestUtilities/Repositories/UserUpdateOnlyRepositoryBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Gauss.Investment.Domain.Entities;
+using Gauss.Investment.Domain.Repositories.User;
+using Moq;
+
+namespace CommonTestUtilities.Repositories
+{
+    public class UserUpdateOnlyRepositoryBuilder
+    {
+        private readonly Mock<IUserUpdateOnlyRepository> _repository;
+
+        public UserUpdateOnlyRepositoryBuilder() => _repository = new Mock<IUserUpdateOnlyRepository>();
+        
+        public UserUpdateOnlyRepositoryBuilder GetById(User user)
+        {
+            _repository.Setup(x => x.GetById(user.Id)).ReturnsAsync(user);
+            return this;
+        }
+
+        public IUserUpdateOnlyRepository Build() => _repository.Object;
+    }
+}
