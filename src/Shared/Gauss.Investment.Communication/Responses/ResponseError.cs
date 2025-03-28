8116fb17257f8b90bb0f3e176eb62a1aa4a20747
Jeferson Almeida <jeffreys.sharp@outlook.com>
2025-03-28 08:51:00 -0300

    feat: Improving documentation

--- src/Shared/Gauss.Investment.Communication/Responses/ResponseError.cs
+++ src/Shared/Gauss.Investment.Communication/Responses/ResponseError.cs
@@ -3,10 +3,10 @@
     public class ResponseError
     {
         public IList<string> Errors { get; set; }
+        public bool TokenIsExpired { get; set; }
 
         public ResponseError(IList<string> errors) => Errors = errors;
 
-        public bool TokenIsExpired { get; set; }
         public ResponseError(string error) 
         {
             Errors = new List<string>
