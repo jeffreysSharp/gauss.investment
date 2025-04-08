ffee8538af03486f37a152d370ccb0f4776be345
Jeferson Almeida <jeffreys.sharp@outlook.com>
2025-04-08 10:13:59 -0300

    feat: Adding unit tests for investment validators

---
+++ tests/CommonTestUtilities/Requests/RequestInvestmentBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using Gauss.Investment.Communication.Enums;
+using Gauss.Investment.Communication.Requests;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestInvestmentBuilder
+    {
+        public static RequestInvestment Build()
+        {
+            return new Faker<RequestInvestment>()
+                .RuleFor(i => i.Title, f => f.Lorem.Word())
+                .RuleFor(i => i.InvestmentCategory, f => f.PickRandom<InvestmentCategory>())
+                .RuleFor(i => i.InvestmentType, f => f.PickRandom<InvestmentType>());
+        }
+    }
+}
---
+++ tests/Validators.Test/Investment/InvestmentValidatorTest.cs
@@ -0,0 +1,93 @@
+﻿using CommonTestUtilities.Requests;
+using FluentAssertions;
+using Gauss.Investment.Application.UseCases.Investment;
+using Gauss.Investment.Exceptions;
+
+namespace Validators.Test.Investment
+{
+    public class InvestmentValidatorTest
+    {
+        [Fact]
+        public void Success()
+        {
+            var validator = new InvestmentValidator();
+
+            var request = RequestInvestmentBuilder.Build();
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Error_Invalid_InvestmentCategory()
+        {
+            var validator = new InvestmentValidator();
+
+            var request = RequestInvestmentBuilder.Build();
+            request.InvestmentCategory = (Gauss.Investment.Communication.Enums.InvestmentCategory?)1000;
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMesssagesException.INVESTMENT_CATEGORY_NOT_SUPPORTED));
+        }
+
+        [Fact]
+        public void Error_Invalid_InvestmentType()
+        {
+            var validator = new InvestmentValidator();
+
+            var request = RequestInvestmentBuilder.Build();
+            request.InvestmentType = (Gauss.Investment.Communication.Enums.InvestmentType?)1000;
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMesssagesException.INVESTMENT_TYPE_NOT_SUPPORTED));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("           ")]
+        [InlineData("")]
+        public void Error_Empty_Tytle(string title)
+        {
+            var validator = new InvestmentValidator();
+
+            var request = RequestInvestmentBuilder.Build();
+            request.Title = title;
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceMesssagesException.INVESTMENT_TITLE_EMPTY));
+        }
+
+        [Fact]
+        public void Success_InvestmentCategory_Null()
+        {
+            var validator = new InvestmentValidator();
+
+            var request = RequestInvestmentBuilder.Build();
+            request.InvestmentCategory = null;
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Success_InvestmentType_Null()
+        {
+            var validator = new InvestmentValidator();
+
+            var request = RequestInvestmentBuilder.Build();
+            request.InvestmentType = null;
+
+            var result = validator.Validate(request);
+
+            result.IsValid.Should().BeTrue();
+        }
+    }
+}
